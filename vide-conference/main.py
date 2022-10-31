6e9dcfa9c1aa9e0218435ac8ed1cfae7e0f62802
dspitsyn <Danylo.Spitsyn@harmonicinc.com>
2022-11-01 00:50:04 +0200

    create a server which transfer video stream between clients

--- vide-conference/main.py
+++ vide-conference/main.py
@@ -1,54 +1,195 @@
+import imp
+from ipaddress import ip_address
+import threading
+from tkinter import E
+from traceback import print_tb
 import cv2
 import socket, imutils, base64
 from io import BytesIO
 import numpy as np
+import argparse
+import time
+import threading
+from multiprocessing import Process,  Pool
+from multiprocessing.pool import AsyncResult
+import os
 
 #Initialize video capture
-cap = cv2.VideoCapture(0)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--file-name', required=True)
+
+opt = parser.parse_args()
+
+file_name = opt.file_name
+print(f"{file_name=}")
+
+WIDTH = 400
+cap = cv2.VideoCapture(file_name)
 #scaling factor
 scaling_factor = 0.5
 # Loop until you hit the Esc key
 
+BUF = 65435
 
-WIDTH = 400
+
 HOST = '146.70.46.20'
 HOST1 = 'localhost'
 PORT = 6666
 
-socket_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT_RETURN = 7666
+
+socket_fd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+socket_fd.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, BUF)
 print("Connecting...")
-socket_fd.connect((HOST, PORT))
+socket_fd.connect((HOST1, PORT))
 print("Cinnected")
+
+
+# socket_return.settimeout(0.03)
 
 def array_to_bytes(x: np.ndarray) -> bytes:
     np_bytes = BytesIO()
     np.save(np_bytes, x, allow_pickle=True)
     return np_bytes.getvalue()
 
- 
-while True:
-    # Capture the current frame
-    ret, frame = cap.read()
-    frame = imutils.resize(frame,width=WIDTH)
-    encoded, buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
-    message = base64.b64encode(buffer)
-    socket_fd.send(message)
-	# frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)    
-    print(f"{len(message)}")
+def return_trafic():
+    socket_return = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_return.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUF)
+    print(f"Binding return socket{(HOST1, PORT_RETURN)=}")
+    try:
+        socket_return.bind((HOST1, PORT_RETURN))
+    except Exception as e:
+        print(f"Error:{e}")
+        print(f"Binding return socket{(HOST1, PORT_RETURN+1)=}")
+        socket_return.bind((HOST1, PORT_RETURN+1))
+        # Capture the current frame 
+        # sockfd, client_address = socket_fd.accept()
 
-# Display the image
-    # socket_fd.send(frame_in_bytes)
+        # print(f"Client {client_address=} was accepted, {sockfd=}")
+        # socket_list.append(sockfd)
+        # t = threading.Thread(name=f"THREAD{sockfd}", target=on_client, args=[f"THREAD{sockfd}" ,sockfd])
+        # # t.setDaemon(True)
+        # t.start()
+    while True:
 
-    # cv2.imshow('Webcam', frame)
-    cv2.imshow('TRANSMITTING VIDEO',frame)
-# Detect if the Esc key has been pressed
-    c = cv2.waitKey(1)
-    if c == 27:
-        break
-# Release the video capture object
-cap.release()
-# Close all active windows
-cv2.destroyAllWindows()
+        print("Waiting for data from server")
+        packet,client_addr = socket_return.recvfrom(BUF)
+       
+        data = base64.b64decode(packet,' /')
+        npdata = np.frombuffer(data,dtype=np.uint8)
+        frame = cv2.imdecode(npdata,1)
+        print("Showing recieved image")
+        cv2.imshow(f"FROM{socket_return}",frame)
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+    cap.release()
+            
+        # 
+        # Close all active windows
+    cv2.destroyAllWindows()
+
+            # print(f"{client_addr[0]=}, {client_addr[1]=}")
+
+            # client_addr_return = client_addr
+            # client_addr_return[1] = 7666
+            # if client_addr_return not in client_list:
+            #     client_list.append(client_addr_return)
+
+    
+
+def procces_video():
+    while True:
+    #     print("Reading video frame")
+    #     # cap.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+        
+        ret, frame = cap.read()
+        print("Frame has been read")
+        frame = imutils.resize(frame,width=WIDTH)
+    #     
+        encoded, buffer = cv2.imencode('.jpg',frame,[cv2.IMWRITE_JPEG_QUALITY,80])
+        message = base64.b64encode(buffer)
+        print("Sending message")
+        socket_fd.sendto(message[:BUF-100], (HOST1, PORT))
+        print("Sleeping")
+        time.sleep(0.03)
+        cv2.imshow('TRANSMITTING VIDEO',frame)
+
+        # cv2.imshow('TRANSMITTING VIDEO',frame)
+    # Detect if the Esc key has been pressed
+        c = cv2.waitKey(1)
+        if c == 27:
+            break
+    # Release the video capture object
+    cap.release()
+    # Close all active windows
+    cv2.destroyAllWindows()
+        
+            # time.sleep(float(0.03))
+            # time.sleep(0.0002)
+
+            # packet,server_addr = socket_fd.recvfrom(BUF)
+            # if packet is not None:
+            #     data = base64.b64decode(packet,' /')
+            #     npdata = np.frombuffer(data,dtype=np.uint8)
+            #     frame = cv2.imdecode(npdata,1)
+            #     cv2.imshow(f"RECEIVING VIDEO From server {socket_fd}",frame)
+            # frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)    
+            # print(f"{len(message[:50000])=}")
+
+        # Display the image
+            # socket_fd.send(frame_in_bytes)
+
+            # cv2.imshow('Webcam', frame)
+        
+    # Detect if the Esc key has been pressed
+
+
+    # Release the video capture object
+
+
+
+
+pid = os.fork()
+
+
+if pid > 0:
+    procces_video()
+       
+else:
+    return_trafic()
+    
+    
+
+
+# while True:
+#     p = Pool(2)
+#     print("Applying return handl")
+#     return_trafic_handelr = p.apply(func=return_trafic)
+#     # return_trafic_handelr.wait(0.04)
+#     print("Applying forward handl")
+#     forward_trafic_handelr = p.apply(func=procces_video)
+#     print("loop iteration done")
+#     # forward_trafic_handelr.wait(0.04)
+    
+
+# p.close()
+# p.join()
+
+# t = Process(target=return_trafic)
+# # t.setDaemon(True)
+# t.start()
+
+# t1 = Process(target=procces_video)
+# t1.start()
+
+# t.join()
+
+# t1.join()
+# t.join()
+
 
 
 # This is server code to send video frames over UDP
--- vide-conference/server.py
+++ vide-conference/server.py
@@ -1,16 +1,23 @@
+from traceback import print_tb
+from py import process
 import imutils
 import cv2
 import socket, base64
 from io import BytesIO
 import numpy as np
 import threading
+import argparse
+import asyncio
 
 HOST = 'localhost'
 PORT = 6666
 
 BUF = 65536
 
-socket_list = []
+loop = asyncio.get_event_loop()
+loop1 = asyncio.get_event_loop()
+
+client_list = set()
 
 def bytes_to_array(b: bytes) -> np.ndarray:
     np_bytes = BytesIO(b)
@@ -18,16 +25,15 @@ def bytes_to_array(b: bytes) -> np.ndarray:
 def bytes_to_array(b: bytes) -> np.ndarray:
     np_bytes = BytesIO(b)
     return np.load(np_bytes, allow_pickle=True)
-
-def on_client(thread_name, socket_client):
-    print(f"{thread_name}, {socket_client=}")
-    while True:
-        print("aboba")
-        packet = socket_client.recv(BUF)
-        data = base64.b64decode(packet,' /')
-        npdata = np.frombuffer(data,dtype=np.uint8)
-        frame = cv2.imdecode(npdata,1)
-        cv2.imshow("RECEIVING VIDEO",frame)
+    
+def on_client(thread_name, packet, client_addr):
+    # print(f"{thread_name}, {socket_client=}")
+   
+    print("aboba")
+    data = base64.b64decode(packet,' /')
+    npdata = np.frombuffer(data,dtype=np.uint8)
+    frame = cv2.imdecode(npdata,1)
+    cv2.imshow(f"RECEIVING VIDEO {thread_name}",frame)
         # try:
         #     for socket in socket_list:
         #         if socket!=socket_client:
@@ -37,44 +43,80 @@ def on_client(thread_name, socket_client):
     # frame = cv2.putText(frame,'FPS: '+str(fps),(10,40),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
    
 
-
-
-if __name__ == '__main__':
-
-    socket_fd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_fd.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUF)
-    socket_fd.bind((HOST, PORT))
-
-    socket_fd.listen(10)
-
-    
-
-    sockfd, client_address = socket_fd.accept()
+def procces_packet(socket_fd):
+    print("Procces packet")
+    i = 0
     while True:
         # Capture the current frame 
-        
-        print(f"Client {client_address=} was accepted, {sockfd=}")
-        socket_list.append(sockfd)
-        t = threading.Thread(name=f"THREAD{sockfd}", target=on_client, args=[f"THREAD{sockfd}" ,sockfd])
-        # t.setDaemon(True)
-        t.start()
+        # sockfd, client_address = socket_fd.accept()
 
+        # print(f"Client {client_address=} was accepted, {sockfd=}")
+        # socket_list.append(sockfd)
+        # t = threading.Thread(name=f"THREAD{sockfd}", target=on_client, args=[f"THREAD{sockfd}" ,sockfd])
+        # # t.setDaemon(True)
+        # t.start()
 
-        # packet = sockfd.recv(BUF)
-        # data = base64.b64decode(packet,' /')
-        # npdata = np.frombuffer(data,dtype=np.uint8)
-        # frame = cv2.imdecode(npdata,1)
-        # cv2.imshow("server VIDEO",frame)
-        # # try:
-        # #     for socket in socket_list:
-        # #         if socket!=socket_client:
-        # #             socket.send(packet)
-        # # except Exception as e:
-        # print(f"Unable to sent on socket:{socket}")
-        
+        print("Waiting for data from user")
+        packet,client_addr = socket_fd.recvfrom(BUF)
+        packet1 = packet
+        print(f"{client_addr[0]=}, {client_addr[1]=}")
+
+        client_addr_return = (client_addr[0], 7666)
+        # client_addr_return[1] = 7666
+        if client_addr_return not in client_list:
+            client_list.add(client_addr_return)
+        else:
+            if i ==0:
+                client_list.add((client_addr[0], 7667))
+            i+=1
+        print(f"{client_list=}")
+            
+
+        print("Data has been recieved")
+        data = base64.b64decode(packet,' /')
+        npdata = np.frombuffer(data,dtype=np.uint8)
+        frame = cv2.imdecode(npdata,1)
+        cv2.imshow(f"RECEIVING VIDEO {client_addr}",frame)
+
+        for client in client_list:
+            if client != client_addr:
+                print(f"Sending video back {client=}")
+                print(f"{(packet==packet1)=}")
+                socket_fd.sendto(packet, client)
     # Detect if the Esc key has been pressed
         c = cv2.waitKey(1)
         if c == 27:
             break
     # Close all active windows
     cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+
+
+
+    socket_fd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    socket_fd.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUF)
+    socket_fd.bind((HOST, PORT))
+    
+    
+
+    # socket_fd.listen(10)
+
+    # sockfd, client_address = socket_fd.accept()
+    # msg,client_addr = socket_fd.recvfrom(BUF)
+    # print('GOT connection from ',client_addr)
+    # if loop1.is_running:
+    #     loop1.stop()
+    # t = threading.Thread( target=lambda: loop1.run_forever())
+    # t.setDaemon(True)
+    # t.start()
+
+    # print("create task")
+    # task = loop.create_task(procces_packet(socket_fd))
+    # loop.run_forever()
+    procces_packet(socket_fd)
+    # asyncio.run(procces_packet(socket_fd))
+   
+
+    
